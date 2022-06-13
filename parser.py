daf11c7496377054794a712e2416cf097e684cc9
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2022-06-13 16:46:29 +0530

    ast node class

--- parser.py
+++ parser.py
@@ -1,3 +1,15 @@
+class ASTNode:
+    def __init__(self, value, typ, children):
+        self.value = value
+        self.type = typ
+        self.children = children
+
+    def __repr__(self, level=0):
+        s = f"ASTNode(\"{self.value}\",{self.type})\n"
+        for child in self.children:
+            s += " " * level + "- " + child.__repr__(level + 1)
+        return s
+
 def match_brackets(x, i=0, parens="()"):
     k = 0
     while i < len(x):
@@ -6,3 +18,13 @@ def match_brackets(x, i=0, parens="()"):
         if k == 0: return i
         i += 1
     return -1  # error
+
+if __name__ == "__main__":
+    n = ASTNode(0,0,[
+        ASTNode(1,0,[ASTNode(3,1,[
+            ASTNode(5,2,[]),
+            ASTNode(6,2,[ASTNode(69,0,[])]),
+        ])]),
+        ASTNode(2,0,[ASTNode(4,1,[])])
+    ])
+    print(n)
